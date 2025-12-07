4fdf0b80b349812d6d2a67ca551959023050854f
FelixVillanuevaCondori <felixeduardobx@gmail.com>
2025-12-07 19:30:08 -0300

    First Commit

---
+++ src/main/java/com/eventos/academicos/AcademicosApplication.java
@@ -0,0 +1,13 @@
+package com.eventos.academicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicosApplication.class, args);
+	}
+
+}
